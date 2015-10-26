d9545656a878fe75cb6057b080dc4b35677e02ac
Florian L <florian@e-lam.net>
2015-10-26 10:53:00 +0100

    Update packages.json

--- recipes/gulp.rb
+++ recipes/gulp.rb
@@ -15,7 +15,15 @@ file 'package.json', <<-JSON
   "browserify-shim": {},
   "browserify": {
     "transform": [
-      "babelify",
+      [
+        "babelify",
+        {
+          "stage": [
+            0
+          ],
+          "optional": "runtime"
+        }
+      ],
       "browserify-shim"
     ]
   },
@@ -25,40 +33,51 @@ file 'package.json', <<-JSON
   "author": "",
   "license": "ISC",
   "dependencies": {
+    "babel-runtime": "^5.8.25",
     "babelify": "^6.3.0",
+    "brfs": "^1.4.1",
     "browserify": "^11.0.1",
     "browserify-shim": "^3.8.10",
     "del": "^1.1.1",
-    "extfs": "0.0.7",
     "gulp": "^3.8.11",
     "gulp-autoprefixer": "^2.2.0",
     "gulp-awspublish": "^3.0.0",
     "gulp-cloudfront": "0.0.14",
+    "gulp-concat": "^2.6.0",
     "gulp-csso": "^1.0.0",
+    "gulp-gzip": "^1.2.0",
     "gulp-iconfont": "^4.0.1",
     "gulp-iconfont-css": "^1.0.1",
     "gulp-imagemin": "^2.3.0",
+    "gulp-notify": "^2.2.0",
     "gulp-plumber": "^1.0.1",
     "gulp-rename": "^1.2.2",
     "gulp-replace": "^0.5.3",
     "gulp-rev-all": "^0.8.21",
     "gulp-sass": "^1.3.3",
     "gulp-size": "^2.0.0",
+    "gulp-streamify": "^1.0.2",
+    "gulp-uglify": "^1.4.1",
     "gulp-util": "^3.0.4",
     "gulp-watch": "^4.3.5",
     "imagemin-pngquant": "^4.2.0",
+    "jquery": "^2.1.4",
+    "lodash": "^3.10.1",
+    "moment": "^2.10.6",
     "normalize.css": "^3.0.3",
+    "nouislider": "^8.0.2",
+    "react-day-picker": "^1.0.5",
     "require-dir": "^0.3.0",
     "run-sequence": "^1.1.2",
     "vinyl-buffer": "^1.0.0",
-    "vinyl-source-stream": "^1.1.0"
+    "vinyl-source-stream": "^1.1.0",
+    "watchify": "^3.4.0"
   },
   "devDependencies": {
-    "browser-sync": "^2.6.9",
-    "gulp-concat": "^2.6.0",
-    "gulp-notify": "^2.2.0"
+    "browser-sync": "^2.6.9"
   }
 }
+
 
 JSON
 
